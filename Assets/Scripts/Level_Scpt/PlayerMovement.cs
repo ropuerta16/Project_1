3a11b64de61a6d3634a7ec4005eee674c2acd1ee
ropuerta16 <rpuertais@edu.tecnocampus.cat>
2025-05-06 11:10:22 +0200

    Player
    Movement, inputs and ground checker

--- Assets/Scripts/Level_Scpt/PlayerMovement.cs
+++ Assets/Scripts/Level_Scpt/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,12 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     public Rigidbody2D rigidbody;
-
+    public float jumpforce = 10f;
     float HorizontalMovement;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f,0.5f);
+    public LayerMask groundLayer;
+
 
     // Update is called once per frame
     void Update()
@@ -21,5 +22,34 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         HorizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(isGrounded())
+        {
+            if (context.performed)
+            {
+                //Hold = full height
+                rigidbody.linearVelocity = new Vector2(rigidbody.linearVelocity.x, jumpforce);
+            }
+            else if (context.canceled)
+            {
+                //Press = low jump
+                rigidbody.linearVelocity = new Vector2(rigidbody.linearVelocity.x, rigidbody.linearVelocity.y * 0.5f);
+            }
+        }
+        
+    }
+
+    private bool isGrounded()
+    {
+        if(Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {  return true; } return false;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawCube(groundCheckPos.position,groundCheckSize);
     }
 }
