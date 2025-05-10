de1015ded7ff437317ec3d82285e2952ba73ead0
ropuerta <puertaroger15@gmail.com>
2025-05-10 23:25:08 +0200

    Tutorial
    giving sound and sound options in the tutorial

--- Assets/Scripts/Level_Scpt/Button_Manager.cs
+++ Assets/Scripts/Level_Scpt/Button_Manager.cs
@@ -6,11 +6,14 @@ public class Button_Manager : MonoBehaviour
 {
     public GameObject Panel_Menu;
     public GameObject Panel_Background;
+    public GameObject Panel_Options;
+    public GameObject Pause_Button;
 
     public void Pause_Click()
     {
         Panel_Menu.SetActive(true);
         Panel_Background.SetActive(true);
+        Pause_Button.SetActive(false);
         Time.timeScale = 0;
     }
 
@@ -23,6 +26,7 @@ public class Button_Manager : MonoBehaviour
     {
         Panel_Menu.SetActive(false);
         Panel_Background.SetActive(false);
+        Pause_Button.SetActive(true);
         Time.timeScale = 1;
     }
 
@@ -32,6 +36,12 @@ public class Button_Manager : MonoBehaviour
     public void Victory_Click()
     {
         LoadScene("LoadScene(\"Level_1\");");
+    }
+
+    public void Option_click()
+    { 
+        Panel_Menu.SetActive(false);
+        Panel_Options.SetActive(true);
     }
 
     public void LoadScene(string sceneName)
