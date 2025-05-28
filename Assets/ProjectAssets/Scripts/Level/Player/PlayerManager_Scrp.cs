de8e83f6f915a25b647c006e49c972865444753c
ropuerta16 <rpuertais@edu.tecnocampus.cat>
2025-05-28 17:56:05 +0200

    Fixed namecode and Level1
    Fixed namecode of the assets and done CountBullets and started sliderHp

---
+++ Assets/ProjectAssets/Scripts/Level/Player/PlayerManager_Scrp.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Timeline;
+using UnityEngine.UI;
+
+public class PlayerManager_Scrp : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    public float jumpforce = 10f;
+    float HorizontalMovement;
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.5f);
+    public LayerMask groundLayer;
+
+    public GameObject Panel_Menu;
+
+    private Collider2D currentGroundCollider2D;
+
+    private bool ActivePanel;
+
+    public ButtonManager_scrp GameOver;
+
+    public float Health = 100f;
+
+    public Animator animator;
+
+    public Transform player;
+
+    void Awake()
+    {
+        ActivePanel = false;
+    }
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(HorizontalMovement * speed, rb.linearVelocity.y);
+
+        animator.SetFloat("XVelocity",rb.linearVelocity.x);
+
+        if (rb.linearVelocity.x < 0)
+            player.localScale = new Vector3(-1, 1, 1);
+        else
+            player.localScale = new Vector3(1,1,1);
+
+        animator.SetFloat("YVelocity",rb.linearVelocity.y);
+
+        if (Health <= 0)
+        { GameOver.GamneOver(); }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        HorizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (isGrounded())
+        {
+            if (context.performed)
+            {
+                //Hold = full height
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpforce);
+            }
+            else if (context.canceled)
+            {
+                //Press = low jump
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+            }
+
+            animator.SetBool("isJumping", true);
+        }
+        else
+        {
+            animator.SetBool("isJumping", false);
+        }
+
+
+    }
+
+    public void Esc(InputAction.CallbackContext context)
+    {
+        if (!ActivePanel)
+        {
+            Panel_Menu.SetActive(true);
+            Time.timeScale = 0;
+            ActivePanel = true;
+        }
+        else if (ActivePanel)
+        {
+            Panel_Menu.SetActive(false);
+            Time.timeScale = 1;
+            ActivePanel = false;
+        }
+    }
+
+
+    private bool isGrounded()
+    {
+        Collider2D GroundCollider = Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer);
+
+        if (GroundCollider)
+        {
+            currentGroundCollider2D = GroundCollider;
+            return true;
+        }
+
+        return false;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawCube(groundCheckPos.position, groundCheckSize);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("BulletEnemy"))
+        { Health -= 10; }
+    }
+
+}
---
+++ Assets/ProjectAssets/Scripts/Level/Player/PlayerShooting_Scrp.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+
+public class PlayerShooting_Scrp : MonoBehaviour
+{
+    public GameObject Bullet;
+    public GameObject BulletSpawner;
+    private float CoolDownShot;
+    public float Maxtime;
+    public float MaxBullet = 10;
+    private float currentBullet;
+    private float CoolDownReload = 2f;
+
+    public TMP_Text Counter;
+
+    void Awake()
+    {
+        currentBullet = MaxBullet;
+        CoolDownShot = Maxtime;
+    }
+    private void Update()
+    {
+        CoolDownShot -= Time.deltaTime;
+
+        if (currentBullet <= 0)
+        { CoolDownReload -= Time.deltaTime; }
+
+        Counter.text = currentBullet.ToString();
+    }
+    public void Shoot(InputAction.CallbackContext context)
+    {
+        if (CoolDownShot <= 0 && currentBullet > 0)
+        {
+            Instantiate(Bullet, BulletSpawner.transform.position, BulletSpawner.transform.rotation);
+            CoolDownShot = Maxtime;
+            currentBullet--;
+        }
+        else if (currentBullet <= 0)
+        {
+            if (CoolDownReload <= 0)
+            {
+                currentBullet = MaxBullet;
+                CoolDownReload = 2f; 
+            }
+        }
+    }
+}
