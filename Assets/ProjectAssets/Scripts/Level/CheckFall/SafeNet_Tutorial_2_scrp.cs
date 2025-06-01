d9fe2e649cb0234b97cec25701847ddd24644291
ropuerta <puertaroger15@gmail.com>
2025-06-01 22:25:59 +0200

    Fixes
    NameSpace Fixes

--- Assets/ProjectAssets/Scripts/Level/CheckFall/SafeNet_Tutorial_2_scrp.cs
+++ Assets/ProjectAssets/Scripts/Level/CheckFall/SafeNet_Tutorial_2_scrp.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class SafeNet_Tutorial_2_scrp : MonoBehaviour
 {
-    public Transform Player;
-    public GameObject Save;
-    public GameObject Shoot;
+    public Transform Players;
+    public GameObject SaveTxt;
+    public GameObject ShootTxt;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Player.transform.position = new Vector2(92f, 1.5f);
+        Players.transform.position = new Vector2(92f, 1.5f);
         StartCoroutine(ExecuteAfterDelay(5f));
     }
 
@@ -16,7 +16,7 @@ public class SafeNet_Tutorial_2_scrp : MonoBehaviour
     IEnumerator ExecuteAfterDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
-        Save.SetActive(false);
-        Shoot.SetActive(true);
+        SaveTxt.SetActive(false);
+        ShootTxt.SetActive(true);
     }
 }
